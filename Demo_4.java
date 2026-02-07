33b563fd9e63d718658dcdbdd28b94b894883ebc
Vishnuvardhan Chirrapu <vishnuvardhanchirrapu@gmail.com>
2026-02-07 23:53:06 +0530

    Add files via upload

---
+++ Demo_4.java
@@ -0,0 +1,12 @@
+// constructer 
+class Circle{
+    Circle(){
+        System.out.println("Hey bro iam a constructor");
+    }
+}
+public class Demo_4 {
+    public static void main(String[] args) {
+        new Circle();
+    }
+}
+
