90afb45e885752323c2ccf73fc6bceecf30ccee0
Vishnuvardhan Chirrapu <vishnuvardhanchirrapu@gmail.com>
2026-02-10 23:13:23 +0530

    Add files via upload

---
+++ Demo_9.java
@@ -0,0 +1,13 @@
+public class Demo_9 {
+    public static void main(String[] args) {
+        if(args.length>0){
+            System.out.println("The command line arguments are :");
+            for(String str : args){
+                System.out.println(str);
+            }
+        }
+        else {
+            System.out.println("No command line arguments are passed !");
+        }
+    }
+}
