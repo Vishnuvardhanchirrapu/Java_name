caffa2fa2105dcdb980380f36100b69d4fd023b1
Vishnuvardhan Chirrapu <vishnuvardhanchirrapu@gmail.com>
2026-02-10 23:13:41 +0530

    Add files via upload

---
+++ Demo_10.java
@@ -0,0 +1,7 @@
+public class Demo_10 {
+    public static void main(String[] args) {
+    
+        int a = Integer.parseInt(args[0]);
+        System.out.println("The integer u entered :"+a);
+    }
+}
